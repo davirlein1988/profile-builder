f8385326dbf7df56562cc1f5ed548e5f104be474
davirlein1988 <lein.davir@gmail.com>
2020-10-05 22:39:10 -0500

    Setup React Router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home, SignIn, SignUp } from "./pages";
 export default function App() {
-  return <div className="App">Main app...</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/signin">
+          <SignIn />
+        </Route>
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
