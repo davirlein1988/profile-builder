a3bb123a2247f206e9ac33b30ffc69f739c47932
davirlein1988 <lein.davir@gmail.com>
2020-10-08 01:01:36 -0500

    Added fallback logo to header

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Header } from "../components";
 import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
 
 export function HeaderContainer({ children }) {
   return (
-    <Header>
-      <h1>Header container</h1>
+    <Header src="2">
+      <Header.Frame>
+        <Header.Logo to={ROUTES.HOME} alt="Profile Builder" src={logo} />
+        <Header.ButtonLink to={ROUTES.SIGN_IN}>sign in</Header.ButtonLink>
+      </Header.Frame>
       {children}
     </Header>
   );
