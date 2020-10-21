abaa697d91b892f59e1ecbbb70b728e677b0f221
davirlein1988 <lein.davir@gmail.com>
2020-10-21 01:17:50 -0500

    Added profile picture to contact

--- src/containers/contact-container.js
+++ src/containers/contact-container.js
@@ -1,18 +1,17 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 
-import { Contact } from "../components";
-
-
+import { Contact } from '../components';
 
 export function ContactContainer() {
-  const [rnName, setRnName ] = useState(""),
-    [rnEmail, setRnEmail ] = useState(""),
-    [rnSubject, setRnSubject ] = useState(""),
-    [rnMessage, setRnMessage ] = useState(""),
-    handleSubmit = e => {
-      e.preventDefault()
-      console.log("formData:", {rnName, rnEmail, rnSubject, rnMessage})
-    }
+  const [rnName, setRnName] = useState(''),
+    [rnEmail, setRnEmail] = useState(''),
+    [rnSubject, setRnSubject] = useState(''),
+    [rnMessage, setRnMessage] = useState(''),
+    handleSubmit = (e) => {
+      e.preventDefault();
+      console.log('formData:', { rnName, rnEmail, rnSubject, rnMessage });
+    },
+    imageProfile = require('../Lein.jpg');
   return (
     <Contact id="contact" className="fix">
       <div className="rn-contact-area ptb--120 bg_color--1">
@@ -23,31 +22,38 @@ export function ContactContainer() {
                 <Contact.TitleWrapper className="section-title text-left mb--50">
                   <Contact.Title className="title" />
                   <Contact.Text className="description">
-                    I am available for freelance work. Connect with me via phone: 
+                    I am available for freelance work. Connect with me via
+                    phone:
                     <a href="tel:+573152503647">3152503647</a> or email:
-                    <a href="mailto:lein.davir@gmail.com">lein.davir@gmail.com</a>
+                    <a href="mailto:lein.davir@gmail.com">
+                      lein.davir@gmail.com
+                    </a>
                   </Contact.Text>
                 </Contact.TitleWrapper>
 
                 <Contact.FormWrapper className="form-wrapper">
                   <Contact.Form
-                  rnName={rnName}
-                  setRnName={setRnName}
-                  rnEmail={rnEmail}
-                  setRnEmail={setRnEmail}
-                  rnSubject={rnSubject}
-                  setRnSubject={setRnSubject}
-                  rnMessage={rnMessage}
-                  setRnMessage={setRnMessage}
-                  handleSubmit={handleSubmit}
-                   />
+                    rnName={rnName}
+                    setRnName={setRnName}
+                    rnEmail={rnEmail}
+                    setRnEmail={setRnEmail}
+                    rnSubject={rnSubject}
+                    setRnSubject={setRnSubject}
+                    rnMessage={rnMessage}
+                    setRnMessage={setRnMessage}
+                    handleSubmit={handleSubmit}
+                  />
                 </Contact.FormWrapper>
-
+              </Contact.Column>
+              <Contact.Column className="col-lg-6 order-1 order-lg-2">
+                <Contact.Box className="thumbnail mb_md--30 mb_sm--30">
+                  <img src={imageProfile} alt="Profile Picture" />
+                </Contact.Box>
               </Contact.Column>
             </Contact.Row>
           </Contact.Box>
         </Contact.Tree>
       </div>
     </Contact>
-  )
+  );
 }
