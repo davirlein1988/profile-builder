2d0062b83855d1226100dad9897817e56119e6fc
davirlein1988 <lein.davir@gmail.com>
2020-11-02 23:49:38 -0500

    added icons for header

--- src/components/header-tree/index.js
+++ src/components/header-tree/index.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import Scrollspy from 'react-scrollspy';
+import { Fix, FiMenu } from 'react-icons/fi';
 import { Container, Box, Row, Col } from './styles/header-tree';
+import {
+  FaTwitter,
+  FaInstagram,
+  FaFacebookF,
+  FaLinkedinIn,
+  FaGithub,
+  FaAt,
+} from 'react-icons/fa';
+
+const SocialShare = [
+  { Social: <FaFacebookF />, link: 'https://facebook.com/lein-davir' },
+  {
+    Social: <FaLinkedinIn />,
+    link: 'https://www.linkedin.com/in/lein-davir-profile/',
+  },
+  { Social: <FaInstagram />, link: 'https://instagram.com/leindavir' },
+  { Social: <FaTwitter />, link: 'https://twitter.com/davirlein' },
+  { Social: <FaGithub />, link: 'https://github.com/davirlein1988' },
+  { Social: <FaAt />, link: 'mailto:lein.davir@gmail.com' },
+];
 
 export default function HeaderTree() {
   return (
